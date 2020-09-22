34594c9f190dfc38ec7c32a9b4b5de61417cdbad
ahuvi brickman <ab583250338@gmail.com>
2020-09-22 12:16:18 +0300

    adding task 6

--- func.py
+++ func.py
@@ -2,10 +2,11 @@
 
 def remove_some_letters(arr):
     letters_to_rm = ['A', 'B', 'C', 'D', 'M', 'N']
+
     new_word = ‘’
     for word in arr:
         for l in word:
-            if l.upper() not in letter_to_rm and ord(l) < 87:
+            if l.upper() not in letter_to_rm and ord(l) < 87 and str.isalnum(l):
                 new_word += l
     return new_word
 print(remove_some_letters([‘BED’]))
