e709f3ad6381b775f5c72add9fb6bd08446d07e8
Pnini-S <pninisalim@gmail.com>
2020-09-22 12:12:54 +0300

    before pull

--- func.py
+++ func.py
@@ -1,6 +1,6 @@
 
 
-<<<<<<< HEAD
+
 def remove_some_letters(arr):
     letters_to_rm = ['A', 'B', 'C', ‘D’]
     new_word = ‘’
@@ -8,6 +8,8 @@ def remove_some_letters(arr):
         for l in word:
             if l not in letter_to_rm and ord(l) < 87:
                 new_word += l
+                if len(new_word) > 7:
+                    raise Exception("TooLong Error")
     return new_word
 print(remove_some_letters([‘BED’]))
 print(remove_some_letters([‘CLOSET’]))
