05843def722c1574d3bc4770f10aef80679ba657
Pnini-S <pninisalim@gmail.com>
2020-09-22 11:52:50 +0300

    change letter

--- func.py
+++ func.py
@@ -1,11 +1,11 @@
 
 
-def x(z):
-    y = ['A', 'B', 'C', ‘D’]
-    d = ‘’
-    for r in z:
-        if r not in y and ord(r) < 87:
-            d += r
-    return d
-print(x(‘BED’))
-print(x(‘CLOSET’))
+def remove_some_letters(word):
+    letters_to_rm = ['A', 'B', 'C', ‘D’]
+    new_word = ‘’
+    for l in word:
+        if l not in letter_to_rm and ord(l) < 87:
+            new_word += l
+    return new_word
+print(remove_some_letters(‘BED’))
+print(remove_some_letters(‘CLOSET’))
