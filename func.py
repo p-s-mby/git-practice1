331d54851fd013cea49448337dffa4b7285aad1e
ahuvi brickman <ab583250338@gmail.com>
2020-09-22 12:06:37 +0300

    task 5

--- func.py
+++ func.py
@@ -5,7 +5,7 @@ def remove_some_letters(arr):
     new_word = ‘’
     for word in arr:
         for l in word:
-            if l not in letter_to_rm and ord(l) < 87:
+            if l.upper() not in letter_to_rm and ord(l) < 87:
                 new_word += l
     return new_word
 print(remove_some_letters([‘BED’]))
